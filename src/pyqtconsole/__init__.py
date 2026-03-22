bd5625b4c366ced2ebf975e8fd5282883f50eb7a
Robert <robert.soor@gmail.com>
2026-03-22 11:13:12 +0100

    TEST - Publishing

--- src/pyqtconsole/__init__.py
+++ src/pyqtconsole/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.3.0"
+__version__ = "1.3.0rc1"
 __description__ = "Lightweight python console, easy to embed into Qt applications"
 __author__ = "Marcus Oskarsson"
 __author_email__ = "marcus.oscarsson@esrf.fr"
